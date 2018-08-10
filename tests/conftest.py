0499c9e6aa7fe422856c4bcaf7968aa6505d498f
teaganryley <teaganschultz@gmail.com>
2018-08-10 13:54:09 -0400

    updated conftest to include two new fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,24 @@
 import os
 import sys
+
+# ensures pytest will locate project without having to run setup.py
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+import pytest
 from src import create_app
+
+
+@pytest.fixture
+def app():
+    """Create and configure a new app instance for each test."""
+    app = create_app({
+        'TESTING': True,
+    })
+
+    yield app
+
+
+@pytest.fixture
+def client(app):
+    """A test client for the app."""
+    return app.test_client()
--- tests/test_factory.py
+++ tests/test_factory.py
@@ -6,3 +6,9 @@ def test_config():
     assert not create_app().testing
     assert create_app({'TESTING': True}).testing
     # is there a way to test if the instance config is loaded?
+
+
+def test_hello(client):
+    response = client.get('/hello')
+    assert response.data == b'Hello, World!'
+
